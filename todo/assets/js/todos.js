af4114b6e24edfe4779bd3bf20e42d215d012be5
dhanishta <48117676+dhanishta@users.noreply.github.com>
2020-09-08 07:19:27 -0600

    Add files via upload

---
+++ todo/assets/js/todos.js
@@ -0,0 +1,29 @@
+// Check off todo list items
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("completed");
+});
+
+
+//Click on the X to delete Todo
+$("ul").on("click", "span", function(event) {
+   $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+   });
+    event.stopPropagation();
+});
+
+$("input[type='text'").keypress(function(event){
+    if(event.which === 13) {
+        //grabbing new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        //create a new li and add to ul
+        $("ul").append(" <li><span><i class='far fa-trash-alt'></i></span>" +  todoText + " <i class='fas fa-arrow-right'></i></li>")
+    }
+});
+
+
+$(".fa-edit").click(function() {
+    $("input[type='text']").focus();
+});
+
